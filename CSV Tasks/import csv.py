8bfaf03b708a3e9e5837ae83ee118db0af72b67d
Totally-1nnocent <rhianalicecrisp@gmail.com>
2025-12-08 10:43:42 +0000

    Update import csv.py

--- CSV Tasks/import csv.py
+++ CSV Tasks/import csv.py
@@ -3,14 +3,23 @@ import csv
 FILENAME = "scores.csv"
 
 def add_score(username, score):
-    with open(FILENAME, "a", newline=""):
+    with open(FILENAME, "a", newline="") as file:
         writer=csv.writer(file)
-        writer.writerow([username][score])
+        writer.writerow([username, score])
 
 def show_scores():
-    with open(FILENAME, "r", newline=""):
-        for line in file:
-            print(line)
+    with open(FILENAME, "r", newline="") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            print(f"{line[0]} scored {line[1]}")
+
+def leaderboarding():
+    leaderboard = {}
+    with open(FILENAME, "r+", newline="") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            leaderboard.update(int(line[1]) : str(line[0]))
+
 
 def main():
     while True:
@@ -21,7 +30,20 @@ def main():
 
         if choice == "1":
             username = input("Enter username: ")
-            score = int(input("Enter score: "))
+            score = input("Enter score: ")
+            while True:
+                try:
+                    int(score)
+                except:
+                    print("Please enter a number. ")
+                    score = input("Enter score: ")
+                else:
+                    score = int(score)
+                    if score > 100 or score < 0:
+                        print("The score must be between 0 and 100.")
+                        score = input("Enter score: ")
+                    else:
+                        break
             add_score(username, score)
         elif choice == "2":
             show_scores()
